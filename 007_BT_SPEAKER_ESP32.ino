970a8f10f952a4525d54c59baf6bc02b791ad932
55987 <88297449+55987@users.noreply.github.com>
2025-10-23 14:24:58 +0800

    Update 007_BT_SPEAKER_ESP32.ino
    更改初始音量，现在初始音量为16（20的8进制）。

--- 007_BT_SPEAKER_ESP32.ino
+++ 007_BT_SPEAKER_ESP32.ino
@@ -1,25 +1,26 @@
 #include "ESP_I2S.h"
 #include "BluetoothA2DPSink.h"
 
-const uint8_t I2S_SCK = 5;       /* Audio data bit clock              BCK端口*/
-const uint8_t I2S_WS = 19;       /* Audio data left and right clock   LCK端口*/
-const uint8_t I2S_SDOUT = 18;    /* ESP32 audio data output (to speakers)   IO端口*/
+const uint8_t I2S_SCK = 5;    /* Audio data bit clock              BCK端口*/
+const uint8_t I2S_WS = 19;    /* Audio data left and right clock   LCK端口*/
+const uint8_t I2S_SDOUT = 18; /* ESP32 audio data output (to speakers)   IO端口*/
 I2SClass i2s;
 
 BluetoothA2DPSink a2dp_sink(i2s);
 
 void setup() {
   pinMode(2, OUTPUT);
-    i2s.setPins(I2S_SCK, I2S_WS, I2S_SDOUT);
-    if (!i2s.begin(I2S_MODE_STD, 44100, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO, I2S_STD_SLOT_BOTH)) {
-      Serial.println("Failed to initialize I2S!");
-      while (1); // do nothing
-    }
+  digitalWrite(2, 1);
 
-    digitalWrite(2, 1);
-    delay(500);
-    digitalWrite(2, 0);
+  i2s.setPins(I2S_SCK, I2S_WS, I2S_SDOUT);
+  if (!i2s.begin(I2S_MODE_STD, 44100, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO, I2S_STD_SLOT_BOTH)) {
+    Serial.println("Failed to initialize I2S!");
+    while (1)
+      ;  // do nothing
+  }
+  a2dp_sink.start("_55987的蓝牙播放器");
+  a2dp_sink.set_volume(20);  //8进制
 
-    a2dp_sink.start("_55987的蓝牙播放器");
+  digitalWrite(2, 0);
 }
 void loop() {}
